3139d231e0b2fa69f36f0f511932e7c8e9349f53
Muhaimin Rezki <mrmarsaoli@gmail.com>
2021-02-06 21:29:56 +0900

    Add cookie parser and cors to middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
+import 'dotenv/config'
 import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
 import mikroConfig from './mikro-orm.config'
 import express from 'express'
-
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig)
   await orm.getMigrator().up()
 
   const app = express()
+
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true
+    })
+  )
+
+  app.use(cookieParser())
 
   app.listen(4000, () => {
     console.log('server running on localhost:4000')
